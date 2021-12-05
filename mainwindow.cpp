c07556942a91ddafe7978c2a10dae350c794ac59
MrNiurh <niu.rh@foxmail.com>
2021-12-05 22:07:12 +0800

    各类对话框

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDialog>
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+: QMainWindow(parent)
+, ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
@@ -18,10 +20,32 @@ MainWindow::MainWindow(QWidget *parent)
         //  dlg.exec();
 
         // 非模态对话框
-        QDialog * dlg2 = new QDialog(this);
-        dlg2->resize(300,200);
-        dlg2->show();
-        dlg2->setAttribute(Qt::WA_DeleteOnClose);
+        //        QDialog * dlg2 = new QDialog(this);
+        //        dlg2->resize(300,200);
+        //        dlg2->show();
+        //        dlg2->setAttribute(Qt::WA_DeleteOnClose);
+
+        // 消息对话框
+        // 错误对话框
+        //QMessageBox::critical(this,"ctitical","错误");
+
+        // 消息对话框
+        // QMessageBox::information(this,"info","信息");
+
+        // 提问对话框
+        // 参数：1.父亲 2.标题 3.提示内容 4.按键类型 5.默认关联回车按键
+        //        if (QMessageBox::Save ==  QMessageBox::question(this,"question","提问",QMessageBox::Save | QMessageBox::Cancel,QMessageBox::Save))
+        //        {
+        //            qDebug()<< "选择的是保存";
+        //        }
+        //        else
+        //        {
+        //            qDebug()<<"选择的是取消";
+        //        }
+
+        // 警告对话框
+        QMessageBox::warning(this,"warning","警告");
+
     });
 }
 
